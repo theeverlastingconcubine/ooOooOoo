6460e5500d1e22d436bba90b92b695259ecaaf10
oOo <oOo@oOo>
2016-12-28 18:13:00 +0100

    00

--- CourseraAlgs022QuickSort/src/Test.java
+++ CourseraAlgs022QuickSort/src/Test.java
@@ -7,7 +7,7 @@ public class Test {
 		Integer[] array = {234,435,1,3556,8753,8,234,45,6,76,3,656, 8,8,34,1,8,8,-55,-555};
 		
 		System.out.println(Arrays.toString(array));
-		System.out.println(array.length + " is the length of an array");
+		System.out.println(array.length + " iss tthe length of an array");
 		
 		System.out.println();
 		
