50d5159a89a71275f933d9800a3b35176405c866
theeverlastingconcubine <oOo@oOo.oOo>
2016-12-29 15:14:43 +0100

    array rotation - to fill

--- CourseraAlgs023MergeSortPoints/src/MergeSortComparator.java
+++ CourseraAlgs023MergeSortPoints/src/MergeSortComparator.java
@@ -1,10 +1,11 @@
+import java.util.Comparator;
 
 public class MergeSortComparator {
 
-	private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi){
+	private static void merge(Object[] a, Object[] aux, Comparator comparator, int lo, int mid, int hi){
 		
-		assert isSorted(a, lo, mid);
-		assert isSorted(a, mid+1, hi);
+		assert isSorted(a, comparator, lo, mid);
+		assert isSorted(a, comparator, mid+1, hi);
 		
 		for(int k = lo; k<=hi; k++){
 			aux[k] = a[k];
@@ -17,46 +18,46 @@ public class MergeSortComparator {
 			
 		if(i>mid){a[k] = aux[j]; j++;}
 		else if (j>hi) {a[k] = aux[i];i++;}
-		else if (less(aux[i], aux[j])) {a[k] = aux[i]; i++;}
+		else if (less(comparator, aux[i], aux[j])) {a[k] = aux[i]; i++;}
 		else {a[k] = aux[j]; j++;}
 		
 		}
 		
-		assert isSorted(a, lo, hi);
+		assert isSorted(a, comparator, lo, hi);
 	}
 
-	public static void sort(Comparable[] a) {
+	public static void sort(Object[] a, Comparator comparator) {
 
-		Comparable[] aux = new Comparable[a.length];
-		sort(a, aux, 0, a.length - 1);
+		Object[] aux = new Object[a.length];
+		sort(a, aux, comparator, 0, a.length - 1);
 
 	}
 	
 	
 
-	private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+	private static void sort(Object[] a, Object[] aux, Comparator comparator, int lo, int hi) {
 
 		if (hi <= lo)
 			return;
 
 		int mid = lo + (hi - lo) / 2;
 
-		sort(a, aux, lo, mid);
-		sort(a, aux, mid + 1, hi);
-		merge(a, aux, lo, mid, hi);
+		sort(a, aux, comparator, lo, mid);
+		sort(a, aux, comparator, mid + 1, hi);
+		merge(a, aux, comparator, lo, mid, hi);
 
 	}
 
-	private static boolean less(Comparable a, Comparable b) {
-		return a.compareTo(b) < 0;
+	private static boolean less(Comparator c, Object a, Object b) {
+		return c.compare(a,b) < 0;
 
 	}
 
-	private static boolean isSorted(Comparable[] a, int from, int to) {
+	private static boolean isSorted(Object[] a, Comparator comparator, int from, int to) {
 
 		int k = 0;
 		for (int i = from; i < to; i++) {
-			if (less(a[i + 1], a[i])) {
+			if (less(comparator, a[i + 1], a[i])) {
 				k++;
 			}
 		}
--- CourseraAlgs023MergeSortPoints/src/Test.java
+++ CourseraAlgs023MergeSortPoints/src/Test.java
@@ -10,66 +10,101 @@ import java.util.Scanner;
 //by computing z coord of a cross (vector) product between corresponding vectors
 //use stack to keep points
 
-
 public class Test {
-	
+
 	public static Point2D[] points;
-	
-	private static void createPoints() throws FileNotFoundException{
-		
+
+	private static void createPoints() throws FileNotFoundException {
+
 		Scanner sc = new Scanner(new File("Points.txt"));
 		points = new Point2D[sc.nextInt()];
-		for(int i = 0; i<points.length; i++){
-			
+		for (int i = 0; i < points.length; i++) {
+
 			points[i] = new Point2D(sc.nextDouble(), sc.nextDouble());
 		}
 		sc.close();
-		
-		
+
 	}
-	
-	private static int ccw(Point2D point1, Point2D point3, Point2D point2){
-		
+
+	private static int ccw(Point2D point1, Point2D point3, Point2D point2) {
+
 		Double x1 = point1.getCoords()[0];
 		Double y1 = point1.getCoords()[1];
 		Double x2 = point2.getCoords()[0];
 		Double y2 = point2.getCoords()[1];
 		Double x3 = point3.getCoords()[0];
 		Double y3 = point3.getCoords()[1];
-		Double ccw = (x2-x1)*(y3-y1)-(x3-x1)*(y2-y1);
-		
-		if (ccw>0) return 1;
-		else if (ccw<0) return -1;
-		else return 0;
-		
-		
+		Double ccw = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1);
+
+		if (ccw > 0)
+			return 1;
+		else if (ccw < 0)
+			return -1;
+		else
+			return 0;
+
 	}
-	
-	public static void main(String[] args) throws FileNotFoundException{
-			
-	/*	Integer[] a = {5,5,76,43,689,5,8,3,5,8899,-10,0,-345,-3,33,100};
-		
-		System.out.println(Arrays.toString(a));
-		System.out.println(a.length);
-		System.out.println();
-		
-		MergeSortComparator.sort(a);
-		System.out.println(Arrays.toString(a));
-		System.out.println();*/
-		
-		
-		createPoints();
-		System.out.println(Arrays.toString(points));	
-		
-		for (int i=0; i<points.length-1; i++){
-		System.out.println(points[i] + "  " + Point2D.BY_POLAR_ANGLE.compare(points[i], points[i+1]) + "  " + points[i+1]);
+
+	private static int indexOfLowestByY(Point2D[] pt) {
+
+		int maxIndex = 0;
+		Point2D max = pt[0];
+
+		for (int i = 0; i < pt.length - 1; i++) {
+			if (Point2D.BY_POLAR_ANGLE.compare(pt[i], pt[i + 1]) < 0) {
+				max = pt[i + 1];
+				maxIndex++;
+			}
 		}
-		
-		
-		
+
+		return maxIndex;
+
+	}
+
+	private static void shiftKEntryToZeroPosition(Object[] a, int k) {
+
+		// we must shift by a.length - k to the right;
+
+		int m = a.length - k;
+		int N = a.length;
+		Object box = new Object();
 
 		
-		
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		/*
+		 * Integer[] a = {5,5,76,43,689,5,8,3,5,8899,-10,0,-345,-3,33,100};
+		 * 
+		 * System.out.println(Arrays.toString(a)); System.out.println(a.length);
+		 * System.out.println();
+		 * 
+		 * MergeSortComparator.sort(a); System.out.println(Arrays.toString(a));
+		 * System.out.println();
+		 */
+
+		createPoints();
+		System.out.println(Arrays.toString(points));
+
+		/*
+		 * for (int i=0; i<points.length-1; i++){ System.out.println(points[i] +
+		 * "  " + Point2D.BY_POLAR_ANGLE.compare(points[i], points[i+1]) + "  "
+		 * + points[i+1]); }
+		 */
+
+		System.out.println();
+
+		MergeSortComparator.sort(points, Point2D.BY_Y_COORDINATE);
+		System.out.println(Arrays.toString(points));
+
+		System.out.println();
+		Integer[] tt = { 0, 1, 2, 3, 4 };
+		System.out.println(Arrays.toString(tt));
+		System.out.println();
+		shiftKEntryToZeroPosition(tt, 2);
+		System.out.println(Arrays.toString(tt));
+
 	}
 
 }
