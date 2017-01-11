3dc71c7f36daa84d15c119d3c79a2f26509cc980
oOo <oOo@oOo>
2017-01-11 14:25:04 +0100

    graps - the beginning

---
+++ CourseraAlgs030GraphAPI/src/Graph.java
@@ -0,0 +1,60 @@
+import java.util.LinkedList;
+
+public class Graph {
+	
+	private int V;
+	private int E;
+	private LinkedList<Integer>[] adj;
+	
+	public Graph(int V){
+		
+		this.V = V;
+		this.E = 0;
+		adj = (LinkedList<Integer>[]) new LinkedList[V];
+		
+		for(int v=0; v<V; v++){
+			
+			adj[v] = new LinkedList<Integer>();
+			
+		}
+				
+	}
+	
+	public void addEdge(int v, int w){
+		
+		adj[v].add(w);
+		adj[w].add(v);
+		E++;
+	}
+	
+	public Iterable<Integer> adj(int v){
+		return adj[v];
+	}
+	
+	public int V(){
+		return V;
+	}
+	public int E(){
+		return E;
+	}
+	
+	
+	public String toString(){
+		
+		String s = V + " verticies, " + E + " edges \n";
+		
+		for (int v=0; v<V; v++){
+			
+			s = s + v + " ";
+			for(int w: adj(v)) s = s + " " + w;				
+			s = s + "\n";
+		}
+		
+		return s;
+	}
+	
+	
+
+	
+
+}
---
+++ CourseraAlgs030GraphAPI/src/Test.java
@@ -0,0 +1,74 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		Scanner sc = new Scanner(new File("testGraph.txt"));
+
+		Graph G = new Graph(sc.nextInt());
+
+		while (sc.hasNextInt()) {
+			G.addEdge(sc.nextInt(), sc.nextInt());
+			}
+		sc.close();
+
+		
+		/*for(int v=0; v<G.V(); v++){
+			for(int w : G.adj(v)){
+				System.out.println(v + " --- " + w);
+			}
+			
+		}*/
+		
+		System.out.println(G.toString());
+		System.out.println("degree of 9 is "+degree(G,9));
+		System.out.println("maxdegree " + maxDegree(G));
+		System.out.println("selfloops "+numberOfSelfLoops(G));
+		
+			
+	}
+	
+	public static int degree(Graph G, int v){
+		
+		int degree = 0;
+		for(int w: G.adj(v)) degree++;
+		return degree;
+	}
+	
+	public static int maxDegree(Graph G){
+		int max = 0;
+		for(int v=0; v<G.V(); v++){
+			if (degree(G,v)>max) max = degree(G,v);
+		}
+		return max;
+		
+	}
+	
+	public static int numberOfSelfLoops(Graph G){
+		
+		int count = 0;
+		for(int v=0; v<G.V(); v++){
+			for(int w: G.adj(v)){
+				if(v==w) count++;
+			}
+				
+		}
+		return count/2;
+	}
+	
+	
+	
+	public static int avgDegree(Graph G){
+		
+		return 2* G.E()/G.V();
+		
+	}
+	
+	
+	
+
+}
