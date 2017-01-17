303b4bb86cb6fb6d97bb6912eab76269e6e73c0a
oOo <oOo@oOo>
2017-01-17 18:10:40 +0100

    0

--- Sample018MinCostPath/src/MinCostpath.java
+++ Sample018MinCostPath/src/MinCostpath.java
@@ -6,8 +6,8 @@ public class MinCostpath {
 	private static int minCost(int[][] a, int m, int n){
 		 
 		if (m==0 && n==0) help[m][n] = a[0][0];
-		else if (m == 0) help[m][n] = a[m][n] * minCost(a, m, n-1);
-		else if (n == 0) help[m][n] = a[m][n] * minCost(a, m-1, n);
+		else if (m == 0) help[m][n] = a[m][n] * a[m][n-1];
+		else if (n == 0) help[m][n] = a[m][n] * a[m-1][n];
 		else help[m][n] = a[m][n] * Math.min(Math.min(minCost(a, m-1, n-1), minCost(a, m-1,n)), minCost(a,m,n-1));
 		 
 		return help[m][n];
@@ -21,6 +21,8 @@ public class MinCostpath {
 						{1,5,3}};
 		
 		System.out.println(minCost(cost,2,2));
+		
+		
 		
 	}
 
