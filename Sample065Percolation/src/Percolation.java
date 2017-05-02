830c4ff840ee12fc7961328a19663e1d9fa3e3d0
theeverlastingconcubine <woodennoise@yandex.ru>
2017-05-02 14:15:42 +0200

    percolation works(!)

--- Sample065Percolation/src/Percolation.java
+++ Sample065Percolation/src/Percolation.java
@@ -1,19 +1,30 @@
+import java.util.Arrays;
 
 public class Percolation {
 	
 	private WQUF qf;
 	private int n;
-	private boolean[][] grid;
+	private int[][] grid;
 	
 	
-	public Percolation(int n){
-		this.n = n;
+	public Percolation(int[][] cells){
+		
+		this.n = cells.length;
 		qf = new WQUF(n*n+2);
+		grid = new int[n][n];
+		
+				
+		for(int i = 0; i<n; i++){
+			for(int j = 0; j<n; j++){
+				if(cells[i][j]==1) open(i,j);
+			}
+		}
+		
 			
 	}
 	
 	private boolean isOpen(int i, int j){
-		return grid[i][j]==true;
+		return grid[i][j]==1;
 	}
 	
 	private int index(int i, int j){
@@ -24,20 +35,34 @@ public class Percolation {
 		
 		int ij = index(i,j);
 		
+		this.grid[i][j]=1;
+		
 		
 		if(i==0) {qf.add(ij, n*n);}
 		else if (i==n-1) {qf.add(ij,n*n+1);}
-		else if (j==0) {if(isOpen(i-1,j)) qf.add(ij, index(i-1,j));}
+		
+		if(i>0 && isOpen(i-1,j)) { qf.add(ij, index(i-1,j)); }
+		if(i<n-1 && isOpen(i+1,j)) {qf.add(ij, index(i+1,j));}
+		if(j>0 && isOpen(i,j-1)) {qf.add(ij, index(i,j-1));}
+		if(j<n-1 && isOpen(i,j+1)) {qf.add(ij, index(i,j+1));}
+		
+		
+		
+		/*else if (j==0) {if(isOpen(i-1,j)) qf.add(ij, index(i-1,j));}
 		else {
 			
 			if(isOpen(i,j-1)) {qf.add(ij, index(i,j-1));}
 			if(isOpen(i-1,j)) {qf.add(ij, index(i-1,j));}
 			
 		
-		}
+		}*/
 		
-		grid[i][j]=true;
 		
+		
+	}
+	
+	public boolean percolates(){
+		return qf.isConnected(n*n, n*n+1);
 	}
 	
 	
--- Sample065Percolation/src/Test.java
+++ Sample065Percolation/src/Test.java
@@ -1,17 +1,66 @@
+import java.util.Random;
+
+import javax.swing.plaf.metal.DefaultMetalTheme;
 
 public class Test {
 	
 	public static void main(String[] args){
 		
-		WQUF qf = new WQUF(6);
+/*		WQUF qf = new WQUF(6);
 		
 		qf.add(0,3);
 		qf.add(2,4);
 		qf.add(5,3);
 		
-		System.out.println(qf.isConnected(2, 5) + "   " + qf.isConnected(0, 5));
+		System.out.println(qf.isConnected(2, 5) + "   " + qf.isConnected(0, 5));*/
 		
+		/*int[][] cells = {{0,1,0,0,1},
+						 {1,0,0,1,0},
+						 {0,1,1,0,1},
+						 {0,0,1,1,0},
+						 {0,0,0,1,0}};*/
+		
+		
+		int[][] cells = new int[100][100];
+		int num = 0;
+		int positive = 0;
+		
+		for(double random = 0.5; random<0.7; random += 0.001){
+		
+		while(num<1000){
+			for(int i = 0; i<cells.length; i++){
+				for(int j = 0; j<cells[0].length; j++){
+					cells[i][j] = random(random);
+				}
+			}
 			
+			Percolation percolation = new Percolation(cells);
+			if(percolation.percolates()) positive++;
+			num++;
+		}
+		
+		double prob = (double)positive/(double)num;
+		num=0;
+		positive=0;
+		
+		System.out.println(prob + "    " + random);
+		
+		if(prob>0.5) {System.out.println(prob + " winner is " + random); break;}
+		
+		}
+				
+		
+		
+	}
+	
+	private static int random(double p){
+		Random rnd = new Random();
+		int hi = (int) Math.round(p*100);
+		
+		int number = rnd.nextInt(100);
+		
+		if(number >=0 && number<=hi) return 1;
+		else return 0;
 		
 		
 	}
